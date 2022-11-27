fb26a294e01f5af1817719adc3ea527b26f1a8fd
Rareș Vasiu <72668860+Rares-AndreiVasiu@users.noreply.github.com>
2022-11-27 21:06:05 +0200

    Update p3.c

--- Lab7/p3.c
+++ Lab7/p3.c
@@ -1,103 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printBits(u_int32_t n)
-{
-    int nBits = sizeof(n) * 8;
 
-    for(int k = nBits - 1; k >= 0; k--)
-    {
-        if((n & (1 << k)) == 0)
-        {
-            putchar('0');
-        }
-        else
-        {
-            putchar('1');
-        }
-    }
 
-    printf("\n");
-}
-
-unsigned getBit(u_int32_t n, unsigned k)
-{
-    return ((n & (1 << k)) >> k);
-}
-
-u_int16_t insertBit(u_int16_t n, int k, unsigned bit)
-{
-    return (n | (bit << k));
-}
-
-u_int16_t evenBitLeftRight(u_int32_t n)
+u_int16_t EveryOrderBit(u_int32_t n)
 {
     u_int16_t newNumber = 0;
 
-    unsigned nBits = sizeof(n) * 8;
-
-    for(int k = nBits - 2; k >= 0; k -= 2)
+    for(int i = 0; i < 32; i += 2)
     {
-        unsigned bit = getBit(n, k);
-        
-        newNumber = insertBit(newNumber, k / 2, bit);
+        printf("pos:%d,  bit: %d\n", i, ((n & (1u << i)) >> i));
 
-        printBits(newNumber);
+        newNumber = newNumber ^ (((n & (1u << i)) >> i) << (i / 2));
     }
 
     return newNumber;
 }
 
-u_int16_t evenBitRightLeft(u_int32_t n)
-{
-    u_int16_t newNumber = 0;
-
-    unsigned nBits = sizeof(n) * 8;
-
-    for(int k = nBits - 2; k >= 0; k -= 2)
-    {
-        unsigned bit = getBit(n, k);
-        
-        newNumber = insertBit(newNumber, k / 2, bit);
-
-        printBits(newNumber);
-    }
-
-    //perform reversing of newNumber
-
-    u_int16_t result = 0;
-
-    while(newNumber)
-    {
-        result <<= 1;
-
-        if((newNumber & 1) == 1)
-        {
-            result ^= 1;
-        }
-
-        newNumber >>= 1;
-    }
-
-    return result;
-}
-
+//1000 0001 1110 1111 1110 0001 1111 0111
+//
 int main()
 {
-    printf("%X\n", evenBitLeftRight(0xFABAFFFF));
+    u_int32_t n = 2179981815;
 
-    printf("%X\n", evenBitRightLeft(0xFABAFFFF));
+    u_int16_t x = EveryOrderBit(n);
 
-    return 0;
+    printf("%d", x); 
 }
-
 /*
-n = FABA FFFF
-1111 1010 1011 1010 1111 1111 1111 1111
-31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9  8  7  6  5  4  3  2  1  0
-1  1   1  1  1  0 1  0  1  0  1  1  1  0  1  0  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1
-I => 1100010011111111
- 
-II => 1111111100100011
-      1111111100100011
+pos:0,  bit: 1
+pos:2,  bit: 1
+pos:4,  bit: 1
+pos:6,  bit: 1
+
+pos:8,  bit: 1
+pos:10,  bit: 0
+pos:12,  bit: 0
+pos:14,  bit: 1
+
+pos:16,  bit: 1
+pos:18,  bit: 1
+pos:20,  bit: 0
+pos:22,  bit: 1
+
+pos:24,  bit: 1
+pos:26,  bit: 0
+pos:28,  bit: 0
+pos:30,  bit: 0
+
+=>0001 1011 1001 1111
+
 */
