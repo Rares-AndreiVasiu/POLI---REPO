e0d3a27bc489b5f48d7e316d3247f8a948356229
Rareș Vasiu <72668860+Rares-AndreiVasiu@users.noreply.github.com>
2023-05-15 21:05:44 +0300

    Add files via upload

--- ProgrammingTechniques/Lab11/main.c
+++ ProgrammingTechniques/Lab11/main.c
@@ -1,53 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 
-#define MAXSIZE 4096
+#define MAXLINES 1000
+#define MAXROWS 1000
+
+typedef struct _point
+{
+    int x;
+    int y;
+
+} point;
+
+char M[MAXLINES][MAXROWS];
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2)
+    FILE *f;
+
+    if (argc <= 1)
     {
-        perror("Not enough arguments from commandline!");
+        printf("there aren't enough arguments to run the programm!");
 
         exit(EXIT_FAILURE);
     }
 
-    char buffer[MAXSIZE];
+    f = fopen(argv[1], "r");
 
-    FILE *fp = fopen(argv[1], "r");
-
-    if (!fp)
+    if (!f)
     {
-        perror("Error while opening the file!");
+        printf("Error while opening the input file!");
 
         exit(EXIT_FAILURE);
     }
 
-    fgets(buffer, MAXSIZE - 1, fp);
+    point start, end;
 
-    const char *ptr = strrchr(buffer, '#');
+    int cols = 0, rows = 0;
 
-    // printf("%s\n", ptr);
+    char firstLine[MAXLINES], buffer[MAXLINES];
 
-    int columns = (ptr - buffer) + 1;
+    fgets(firstLine, MAXLINES - 1, f);
 
-    int rows = 0;
+    cols = strrchr(firstLine, '#') - firstLine + 1;
 
-    char maze[columns][columns]; //we define the maze
+    printf("Cols: %d\n", cols);
 
-    strncpy(maze[rows++], buffer, columns);
+    strncpy(M[rows++], firstLine, cols);
 
-    while(fgets(buffer, columns, fp) != EOF)
+    // printf("%s\n\n", M[1]);
+
+    while (!feof(f))
     {
-        strncpy(maze[rows++], buffer, columns);
+        if (fgets(buffer, cols, f) != NULL)
+        {
+            strncpy(M[rows++], buffer, cols);
+
+            if(strchr(M[rows - 1], 'E'))
+            {
+                puts("We have found the end pos!");
+
+                printf("At line: %d\n", rows - 1);
+            }
+        }
+        else
+        {
+            printf("Error while reading the file!");
+
+            exit(EXIT_FAILURE);
+        }
     }
 
-    for(int i = 0; i < rows; ++ i)
+    printf("Rows: %d\n", rows);
+
+    for (int i = 0; i < rows; ++i)
     {
-        printf("%s\n", maze[i]);
+        for (int j = 0; j < cols; ++j)
+        {
+            printf("%c", M[i][j]);
+
+            if(M[i][j] == 'S')
+            {
+                start.x = i;
+                start.y = j;
+            }
+
+            if (M[i][j] == 'E')
+            {
+                end.x = i;
+
+                end.y = j;
+
+                printf("\n%d %d\n", end.x, end.y);
+                return 0;
+            }
+        }
     }
+
+    printf("%s\n", M[start.x]);
+
+    printf("\nStart: %d %d, End: %d %d\n", start.x, start.y, end.x, end.y);
 
     return 0;
 }
